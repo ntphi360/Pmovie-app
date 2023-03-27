510c01fc2e16293e1e3d3af7e3926b64caae1a51
Phi <112464779+ntphi360@users.noreply.github.com>
2023-03-27 16:29:03 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvdTpsrmGVRuHy1jbw0zUzJ4ktMzKcWkE",
-  authDomain: "netflix-db-40d3a.firebaseapp.com",
-  projectId: "netflix-db-40d3a",
-  storageBucket: "netflix-db-40d3a.appspot.com",
-  messagingSenderId: "988671642015",
-  appId: "1:988671642015:web:ac0baced80ccbcea7315aa"
+  apiKey: "AIzaSyBEm8m9qFr15xjJ7F58eiqzfgtkinOjV-g",
+  authDomain: "movie-db-2-3d4b6.firebaseapp.com",
+  projectId: "movie-db-2-3d4b6",
+  storageBucket: "movie-db-2-3d4b6.appspot.com",
+  messagingSenderId: "306212343060",
+  appId: "1:306212343060:web:4b6d3ec1913c5c93a94601"
 };
 
 export const app = initializeApp(firebaseConfig);
